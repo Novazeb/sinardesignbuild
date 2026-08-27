a8279ceaa85ff27e94ca7645ef1ec5a95be75831
Novazeb <novaberkatsyukurzebua@gmail.com>
2026-08-27 09:34:00 +0700

    feat(team): add team carousel featuring Nova Zebua and architectural experts

---
+++ src/components/TeamSection.vue
@@ -0,0 +1,83 @@
+<template>
+  <section class="section novi-background bg-cover section-lg bg-gray-100 text-center" id="team">
+    <div class="container">
+      <h3 class="wow fadeInLeft">Tim Profesional Kami</h3>
+      <h6 class="title-style-1 wow fadeInRight">Para ahli di balik mahakarya SINAR DESIGN &amp; BUILD</h6>
+      <!-- Owl Carousel-->
+      <div class="owl-carousel" data-items="1" data-sm-items="2" data-md-items="3" data-xl-items="4" data-margin="5" data-dots="true" data-mouse-drag="false">
+        <article class="team-classic wow fadeInUp" data-wow-delay=".1s">
+          <div class="team-classic-figure"><img src="/images/team-1-290x284.jpg" alt="Nova Berkatsyukur Zebua" width="290" height="284"/>
+          </div>
+          <div class="team-classic-body">
+            <h5 class="team-classic-name"><a href="https://instagram.com/zebua_1011" target="_blank">Nova B. Zebua</a></h5>
+          </div>
+          <div class="team-classic-footer">
+            <div class="team-classic-status">Principal Architect &amp; Founder</div>
+            <div class="team-classic-list-panel">
+              <ul class="team-classic-list-social list-inline">
+                <li><a class="icon novi-icon mdi mdi-instagram" href="https://instagram.com/zebua_1011" target="_blank"></a></li>
+                <li><a class="icon novi-icon mdi mdi-email" href="mailto:novaberkatsyukurzebua@gmail.com"></a></li>
+              </ul>
+              <div class="heading-1 team-classic-placeholder">Nova</div>
+            </div>
+          </div>
+        </article>
+        <article class="team-classic wow fadeInUp">
+          <div class="team-classic-figure"><img src="/images/team-2-290x284.jpg" alt="Senior Architect" width="290" height="284"/>
+          </div>
+          <div class="team-classic-body">
+            <h5 class="team-classic-name"><a href="#">Bima Pratama, S.Ars</a></h5>
+          </div>
+          <div class="team-classic-footer">
+            <div class="team-classic-status">Senior Design Architect</div>
+            <div class="team-classic-list-panel">
+              <ul class="team-classic-list-social list-inline">
+                <li><a class="icon novi-icon mdi mdi-instagram" href="https://instagram.com/zebua_1011" target="_blank"></a></li>
+                <li><a class="icon novi-icon mdi mdi-email" href="mailto:novaberkatsyukurzebua@gmail.com"></a></li>
+              </ul>
+              <div class="heading-1 team-classic-placeholder">Bima</div>
+            </div>
+          </div>
+        </article>
+        <article class="team-classic wow fadeInUp" data-wow-delay=".05s">
+          <div class="team-classic-figure"><img src="/images/team-3-290x284.jpg" alt="Interior Designer" width="290" height="284"/>
+          </div>
+          <div class="team-classic-body">
+            <h5 class="team-classic-name"><a href="#">Siti Rahmadani, S.Ds</a></h5>
+          </div>
+          <div class="team-classic-footer">
+            <div class="team-classic-status">Lead Interior Designer</div>
+            <div class="team-classic-list-panel">
+              <ul class="team-classic-list-social list-inline">
+                <li><a class="icon novi-icon mdi mdi-instagram" href="https://instagram.com/zebua_1011" target="_blank"></a></li>
+                <li><a class="icon novi-icon mdi mdi-email" href="mailto:novaberkatsyukurzebua@gmail.com"></a></li>
+              </ul>
+              <div class="heading-1 team-classic-placeholder">Siti</div>
+            </div>
+          </div>
+        </article>
+        <article class="team-classic wow fadeInUp" data-wow-delay=".15s">
+          <div class="team-classic-figure"><img src="/images/team-4-290x284.jpg" alt="Project Manager" width="290" height="284"/>
+          </div>
+          <div class="team-classic-body">
+            <h5 class="team-classic-name"><a href="#">Ir. Denny Setiawan</a></h5>
+          </div>
+          <div class="team-classic-footer">
+            <div class="team-classic-status">Project &amp; Site Manager</div>
+            <div class="team-classic-list-panel">
+              <ul class="team-classic-list-social list-inline">
+                <li><a class="icon novi-icon mdi mdi-instagram" href="https://instagram.com/zebua_1011" target="_blank"></a></li>
+                <li><a class="icon novi-icon mdi mdi-email" href="mailto:novaberkatsyukurzebua@gmail.com"></a></li>
+              </ul>
+              <div class="heading-1 team-classic-placeholder">Denny</div>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
+
