241e9f2be45365bf051285ae934bfd9132e5e165
Novazeb <novaberkatsyukurzebua@gmail.com>
2026-08-27 09:34:01 +0700

    feat(blog): implement latest architectural insights and educational posts

---
+++ src/components/BlogSection.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="section novi-background bg-cover section-xl bg-white text-center decoration-1">
+    <div class="container">
+      <h3 class="wow fadeInRight">Inspirasi &amp; Edukasi<br>Dunia Rancang Bangun</h3>
+      <h6 class="title-style-1 wow fadeInLeft">Artikel &amp; Wawasan Terkini</h6>
+      <div class="row row-xl row-30 justify-content-center justify-content-xl-between">
+        <div class="col-sm-6 col-md-4 col-xl-3 wow fadeInUp" data-wow-delay=".05s">
+          <article class="post-classic">
+            <div class="heading-3 post-classic-time">
+              <time datetime="2024-06-19">19 Juni 2024</time>
+            </div>
+            <a class="post-classic-figure" href="#">
+              <img src="/images/post-1-270x182.jpg" alt="Tren Desain Rumah Tropis Modern" width="270" height="182"/>
+            </a>
+            <h5 class="post-classic-title"><a href="#">Tren Desain Rumah Tropis Modern di Perkotaan</a></h5>
+            <p class="post-classic-text">Memaksimalkan sirkulasi udara alami dan pencahayaan efektif untuk menciptakan hunian yang sejuk dan hemat energi...</p>
+          </article>
+        </div>
+        <div class="col-sm-6 col-md-4 col-xl-3 wow fadeInUp">
+          <article class="post-classic">
+            <div class="heading-3 post-classic-time">
+              <time datetime="2024-05-30">30 Mei 2024</time>
+            </div>
+            <a class="post-classic-figure" href="#">
+              <img src="/images/post-2-270x182.jpg" alt="Panduan Estimasi Biaya Bangun Rumah" width="270" height="182"/>
+            </a>
+            <h5 class="post-classic-title"><a href="#">Panduan Menghitung Estimasi Biaya Bangun Rumah</a></h5>
+            <p class="post-classic-text">Langkah cerdas menyusun Rencana Anggaran Biaya (RAB) transparan, pemilihan material bermutu, dan efisiensi konstruksi...</p>
+          </article>
+        </div>
+        <div class="col-sm-6 col-md-4 col-xl-3 wow fadeInUp" data-wow-delay=".05s">
+          <article class="post-classic">
+            <div class="heading-3 post-classic-time">
+              <time datetime="2024-04-15">15 April 2024</time>
+            </div>
+            <a class="post-classic-figure" href="#">
+              <img src="/images/post-3-270x182.jpg" alt="Kerjasama dengan Arsitek Profesional" width="270" height="182"/>
+            </a>
+            <h5 class="post-classic-title"><a href="#">Keuntungan Menggunakan Jasa Arsitek Profesional</a></h5>
+            <p class="post-classic-text">Bagaimana arsitek profesional merancang tata ruang yang fungsional, struktur tahan lama, serta bernilai investasi tinggi...</p>
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
+
