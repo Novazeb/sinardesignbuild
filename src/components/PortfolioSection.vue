b304342b14ff0c3f7613aea13061828e7f407a19
Novazeb <novaberkatsyukurzebua@gmail.com>
2026-08-27 09:34:01 +0700

    feat(portfolio): add project gallery carousel with LightGallery preview

---
+++ src/components/PortfolioSection.vue
@@ -0,0 +1,86 @@
+<template>
+  <section class="section novi-background bg-cover section-lg section-inset-2 bg-gray-100" id="portfolio">
+    <div class="container">
+      <div class="caption-classic">
+        <div class="caption-classic-group">
+          <h1 class="caption-classic-title wow fadeInLeft">Porto<span class="font-weight-light">folio</span></h1>
+          <p class="caption-classic-text wow fadeInRight">Jelajahi berbagai proyek arsitektur dan hunian modern unggulan yang telah kami selesaikan dengan standar kualitas terbaik di Jakarta dan sekitarnya.</p>
+        </div>
+        <div class="caption-classic-decor wow blurIn"></div>
+      </div>
+      <!-- Owl Carousel-->
+      <div class="owl-carousel owl-style-1" data-autoplay="false" data-items="1" data-margin="30" data-xl-margin="90" data-nav="true" data-dots="true" data-mouse-drag="false" data-smart-speed="600">
+        <article class="project-mary">
+          <div class="project-mary-figure">
+            <img src="/images/project-1-775x524.jpg" alt="Rumah Modern Minimalis Jakarta Selatan" width="775" height="524"/>
+            <div class="project-mary-link-wrap">
+              <a class="project-mary-link mdi mdi-magnify" data-lightgallery="item" href="/images/project-1-1200x800-original.jpg">
+                <img src="/images/project-1-775x524.jpg" alt="" width="775" height="524"/>
+              </a>
+            </div>
+          </div>
+          <div class="project-mary-content">
+            <ul class="project-mary-panel">
+              <li class="project-mary-panel-item">
+                <time class="project-mary-time" datetime="2024-03-15">15 Maret 2024</time>
+              </li>
+              <li class="project-mary-panel-item">
+                <div class="project-mary-tag">Arsitektur &amp; Bangun Baru</div>
+              </li>
+            </ul>
+            <h3 class="project-mary-title"><a href="#">Hunian Tropis Modern - Pondok Indah, Jakarta Selatan</a></h3>
+            <p class="project-mary-text">Pembangunan rumah tinggal 2 lantai dengan ventilasi silang optimal, fasad berkarakter, dan penataan ruang keluarga yang luas serta elegan.</p>
+          </div>
+        </article>
+        <article class="project-mary">
+          <div class="project-mary-figure">
+            <img src="/images/project-2-775x524.jpg" alt="Vila Kontemporer Jakarta Barat" width="775" height="524"/>
+            <div class="project-mary-link-wrap">
+              <a class="project-mary-link mdi mdi-magnify" data-lightgallery="item" href="/images/project-2-1200x800-original.jpg">
+                <img src="/images/project-2-775x524.jpg" alt="" width="775" height="524"/>
+              </a>
+            </div>
+          </div>
+          <div class="project-mary-content">
+            <ul class="project-mary-panel">
+              <li class="project-mary-panel-item">
+                <time class="project-mary-time" datetime="2024-06-19">19 Juni 2024</time>
+              </li>
+              <li class="project-mary-panel-item">
+                <div class="project-mary-tag">Desain Eksterior &amp; Kolam</div>
+              </li>
+            </ul>
+            <h3 class="project-mary-title"><a href="#">Vila Kontemporer &amp; Kolam Renang - Jakarta Barat</a></h3>
+            <p class="project-mary-text">Perpaduan fasad modern dengan aksen kisi-kisi kayu komposit, pencahayaan dramatis, dan area santai semi-outdoor yang asri.</p>
+          </div>
+        </article>
+        <article class="project-mary">
+          <div class="project-mary-figure">
+            <img src="/images/project-3-775x524.jpg" alt="Ruang Usaha Komersial Jakarta Pusat" width="775" height="524"/>
+            <div class="project-mary-link-wrap">
+              <a class="project-mary-link mdi mdi-magnify" data-lightgallery="item" href="/images/project-3-1200x800-original.jpg">
+                <img src="/images/project-3-775x524.jpg" alt="" width="775" height="524"/>
+              </a>
+            </div>
+          </div>
+          <div class="project-mary-content">
+            <ul class="project-mary-panel">
+              <li class="project-mary-panel-item">
+                <time class="project-mary-time" datetime="2024-01-10">10 Januari 2024</time>
+              </li>
+              <li class="project-mary-panel-item">
+                <div class="project-mary-tag">Renovasi &amp; Komersial</div>
+              </li>
+            </ul>
+            <h3 class="project-mary-title"><a href="#">Creative Studio &amp; Kantor Modern - Jakarta Pusat</a></h3>
+            <p class="project-mary-text">Transformasi total bangunan komersial menjadi ruang kerja inspiratif dengan konsep open-space dan tata cahaya alami melimpah.</p>
+          </div>
+        </article>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
+
