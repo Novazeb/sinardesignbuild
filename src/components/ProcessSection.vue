6e6ac26d96f4795fb78a307a325c4363d4bed4f4
Novazeb <novaberkatsyukurzebua@gmail.com>
2026-08-27 09:34:00 +0700

    feat(process): create 4-step tabbed construction workflow component

---
+++ src/components/ProcessSection.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="section novi-background bg-cover section-xl bg-gray-800 decoration-1">
+    <div class="tabs-custom container" id="tabs-1" data-view-triggerable="true">
+      <div class="row row-30 row-md-40 justify-content-lg-between">
+        <div class="col-lg-5">
+          <h6 class="title-style-1 wow fadeInLeft">4 Langkah Menuju Bangunan Impian</h6>
+          <h1 class="wow fadeInLeft" data-wow-delay=".05s">Proses<br class="d-none d-lg-block">Kerja <span class="font-weight-light">Kami</span></h1>
+          <!-- Tab panes-->
+          <div class="tab-content wow fadeInLeft" data-wow-delay=".1s">
+            <div class="tab-pane fade" id="tabs-1-1">
+              <h3 class="title-style-1">Konsultasi &amp; Pemetaan Kebutuhan</h3>
+              <p>Langkah awal dimulai dengan berdiskusi mendalam mengenai kebutuhan ruang, preferensi estetika, dan estimasi anggaran proyek Anda. Kami siap mendengarkan aspirasi serta memberikan masukan profesional.</p>
+              <p>Tahap ini sangat krusial agar tim arsitek kami memahami karakteristik lahan di Jakarta dan sekitarnya, serta menyusun rencana strategis yang efisien dan tepat sasaran.</p>
+            </div>
+            <div class="tab-pane fade" id="tabs-1-2">
+              <h3 class="title-style-1">Pengembangan Konsep &amp; Desain 3D</h3>
+              <p>Kami merancang tata letak denah yang fungsional, memodelkan visualisasi 3D fotorealistis, dan menentukan palet material terbaik untuk mewujudkan karakter bangunan Anda.</p>
+              <p>Seluruh gambar kerja teknis (DED) dan Rencana Anggaran Biaya (RAB) disusun secara detail, transparan, dan memenuhi standar keamanan serta efisiensi energi.</p>
+            </div>
+            <div class="tab-pane fade" id="tabs-1-3">
+              <h3 class="title-style-1">Pelaksanaan Konstruksi Presisi</h3>
+              <p>Pembangunan fisik dilaksanakan oleh tenaga kerja terampil dengan pemilihan material premium dan diawasi langsung oleh arsitek serta manajer proyek berpengalaman.</p>
+              <p>Mulai dari pekerjaan pondasi kokoh, struktur bangunan, hingga pekerjaan arsitektural eksterior dan interior dijalankan dengan pengawasan mutu yang ketat.</p>
+            </div>
+            <div class="tab-pane fade show active" id="tabs-1-4">
+              <h3 class="title-style-1">Inspeksi Akhir &amp; Serah Terima Kunci</h3>
+              <p>Memasuki tahap finalisasi, tim pengawas melakukan uji kelayakan menyeluruh (quality checklist) untuk memastikan seluruh detail pengerjaan rapi dan berfungsi optimal.</p>
+              <p>Kami mengajak Anda melakukan peninjauan akhir bersama sebelum proses serah terima kunci, lengkap dengan garansi pemeliharaan struktural.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-7 col-xl-6">
+          <div class="inset-xl-left-35">
+            <ul class="nav nav-style-1">
+              <li class="nav-item wow blurIn" role="presentation"><a class="nav-link active" href="#tabs-1-1" data-toggle="tab">01. Konsultasi &amp; Kebutuhan</a></li>
+              <li class="nav-item wow blurIn" role="presentation" data-wow-delay=".05s"><a class="nav-link" href="#tabs-1-2" data-toggle="tab">02. Konsep Desain &amp; 3D</a></li>
+              <li class="nav-item wow blurIn" role="presentation" data-wow-delay=".1s"><a class="nav-link" href="#tabs-1-3" data-toggle="tab">03. Konstruksi &amp; Bangun</a></li>
+              <li class="nav-item wow blurIn" role="presentation" data-wow-delay=".15s"><a class="nav-link" href="#tabs-1-4" data-toggle="tab">04. Finishing &amp; Serah Terima</a></li>
+            </ul>
+            <div class="project-creative wow blurIn">
+              <div class="project-creative-figure active" data-view-trigger="#tabs-1-1"><img src="/images/index-1-531x327.jpg" alt="" width="531" height="327"/>
+              </div>
+              <div class="project-creative-figure" data-view-trigger="#tabs-1-2"><img src="/images/index-2-531x327.jpg" alt="" width="531" height="327"/>
+              </div>
+              <div class="project-creative-figure" data-view-trigger="#tabs-1-3"><img src="/images/index-3-531x327.jpg" alt="" width="531" height="327"/>
+              </div>
+              <div class="project-creative-figure" data-view-trigger="#tabs-1-4"><img src="/images/index-4-531x327.jpg" alt="" width="531" height="327"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
+
