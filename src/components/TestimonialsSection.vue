8a47580dd665f68c964ec45b6a0933548e6300e2
Novazeb <novaberkatsyukurzebua@gmail.com>
2026-08-27 09:34:01 +0700

    feat(testimonials): add client reviews carousel and partner badges

---
+++ src/components/TestimonialsSection.vue
@@ -0,0 +1,83 @@
+<template>
+  <section class="section novi-background bg-cover section-xl section-inset-2 bg-gray-800 bg-image" style="background-image: url(/images/bg-image-1.jpg)">
+    <div class="container">
+      <!-- Owl Carousel-->
+      <div class="owl-carousel owl-style-2" data-loop="true" data-autoplay="true" data-items="1" data-margin="30" data-dots="true" data-mouse-drag="false" data-animation-in="fadeIn" data-animation-out="fadeOut">
+        <article class="quote-classic">
+          <div class="quote-classic-text">
+            <div class="q">Dari sekian banyak konsultan arsitektur dan kontraktor di Jakarta, SINAR DESIGN &amp; BUILD benar-benar unggul dalam hal kepedulian detail, ketepatan waktu, dan hasil rancang bangun yang sangat memuaskan.</div>
+          </div>
+          <div class="unit unit-spacing-md align-items-center text-left d-inline-flex">
+            <div class="unit-left">
+              <div class="quote-classic-figure">
+                <img src="/images/user-1-80x80.jpg" alt="Bpk. Hendra Wijaya" width="80" height="80"/>
+              </div>
+            </div>
+            <div class="unit-body">
+              <div class="quote-classic-author">Hendra Wijaya</div>
+              <div class="quote-classic-status">Pengusaha Properti, Jakarta</div>
+            </div>
+          </div>
+          <div class="quote-classic-decor-wrap">
+            <div class="quote-classic-decor"></div>
+            <div class="quote-classic-decor"></div>
+          </div>
+        </article>
+        <article class="quote-classic">
+          <div class="quote-classic-text">
+            <div class="q">Saya mencari tim yang tidak hanya paham estetika modern tapi juga perhitungan struktur dan RAB yang transparan. Seluruh kebutuhan keluarga kami diakomodasi dengan sangat rapi dan profesional.</div>
+          </div>
+          <div class="unit unit-spacing-md align-items-center text-left d-inline-flex">
+            <div class="unit-left">
+              <div class="quote-classic-figure">
+                <img src="/images/user-2-80x80.jpg" alt="Ibu Rina Paramita" width="80" height="80"/>
+              </div>
+            </div>
+            <div class="unit-body">
+              <div class="quote-classic-author">Rina Paramita</div>
+              <div class="quote-classic-status">Pemilik Rumah, Jakarta Selatan</div>
+            </div>
+          </div>
+          <div class="quote-classic-decor-wrap">
+            <div class="quote-classic-decor"></div>
+            <div class="quote-classic-decor"></div>
+          </div>
+        </article>
+        <article class="quote-classic">
+          <div class="quote-classic-text">
+            <div class="q">Saya memilih SINAR DESIGN &amp; BUILD karena portofolio arsitekturnya yang sangat kuat dan berkarakter. Hasil renovasi dan perluasan rumah kami jauh melebihi ekspektasi. Sangat kami rekomendasikan!</div>
+          </div>
+          <div class="unit unit-spacing-md align-items-center text-left d-inline-flex">
+            <div class="unit-left">
+              <div class="quote-classic-figure">
+                <img src="/images/user-3-80x80.jpg" alt="Bpk. Arya Pratama" width="80" height="80"/>
+              </div>
+            </div>
+            <div class="unit-body">
+              <div class="quote-classic-author">Arya Pratama</div>
+              <div class="quote-classic-status">Eksekutif Bisnis, Jakarta Pusat</div>
+            </div>
+          </div>
+          <div class="quote-classic-decor-wrap">
+            <div class="quote-classic-decor"></div>
+            <div class="quote-classic-decor"></div>
+          </div>
+        </article>
+      </div>
+      <div class="inset-md-left-custom-1 text-sm-left">
+        <h5 class="title-style-2">Dipercaya Oleh</h5>
+        <div class="clients-classic-wrap wow fadeInUp">
+          <a class="clients-classic" href="#"><img src="/images/clients-1-87x54.png" alt="" width="87" height="54"/></a>
+          <a class="clients-classic" href="#"><img src="/images/clients-2-112x45.png" alt="" width="112" height="45"/></a>
+          <a class="clients-classic" href="#"><img src="/images/clients-3-146x28.png" alt="" width="146" height="28"/></a>
+          <a class="clients-classic" href="#"><img src="/images/clients-4-84x47.png" alt="" width="84" height="47"/></a>
+          <a class="clients-classic" href="#"><img src="/images/clients-5-131x29.png" alt="" width="131" height="29"/></a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
+
