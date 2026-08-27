adce189c9dd78dd5977878cde713922f01f1e87f
Novazeb <novaberkatsyukurzebua@gmail.com>
2026-08-27 09:34:00 +0700

    feat(hero): create interactive Horace swiper hero slider showcasing Jakarta projects

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,68 @@
+<template>
+  <section class="section novi-background bg-cover section-horace">
+    <div class="section-horace-left bg-gray-800">
+      <div class="section-horace-left-content text-center">
+        <article class="project-classic project-classic-2">
+          <div class="project-classic-title">Desain<br>&amp; Bangun</div>
+          <h6 class="project-classic-text">Hunian Modern &amp;<br>Efisien Energi<br>Mulai 150 m²</h6>
+          <a class="button button-jerry button-primary-outline" href="#contacts">
+            Hubungi Kami
+            <span class="button-jerry-line"></span>
+            <span class="button-jerry-line"></span>
+            <span class="button-jerry-line"></span>
+            <span class="button-jerry-line"></span>
+          </a>
+        </article>
+      </div>
+      <div class="section-horace-left-panel">
+        <div class="swiper-custom-nav">
+          <!-- Swiper Navigation-->
+          <div class="swiper-button-prev mdi mdi-arrow-left" id="swiper-prev"></div>
+          <div class="swiper-button-next mdi mdi-arrow-right" id="swiper-next"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Swiper-->
+    <div class="swiper-container swiper-slider swiper-slider-1" data-loop="true" data-autoplay="5000" data-simulate-touch="false" data-custom-prev="#swiper-prev" data-custom-next="#swiper-next" data-slide-effect="fade">
+      <div class="swiper-wrapper text-center">
+        <div class="swiper-slide context-dark" data-slide-bg="images/slide-1-1339x729.jpg">
+          <div class="swiper-slide-caption project-modern">
+            <ul class="project-modern-info">
+              <li class="heading-6 project-modern-info-item project-modern-address" data-caption-animate="fadeInDown" data-caption-delay="200">Jakarta Selatan</li>
+              <li class="heading-6 project-modern-info-item" data-caption-animate="fadeInDown" data-caption-delay="100">200 m²</li>
+              <li class="heading-6 project-modern-info-item" data-caption-animate="fadeInDown" data-caption-delay="0">Modern Tropis</li>
+            </ul>
+            <div class="project-modern-decor" data-caption-animate="fadeInRight" data-caption-delay="0"></div>
+          </div>
+        </div>
+        <div class="swiper-slide context-dark" data-slide-bg="images/slide-2-1339x729.jpg">
+          <div class="swiper-slide-caption project-modern">
+            <ul class="project-modern-info">
+              <li class="heading-6 project-modern-info-item project-modern-address" data-caption-animate="fadeInDown" data-caption-delay="200">Jakarta Barat</li>
+              <li class="heading-6 project-modern-info-item" data-caption-animate="fadeInDown" data-caption-delay="100">290 m²</li>
+              <li class="heading-6 project-modern-info-item" data-caption-animate="fadeInDown" data-caption-delay="0">Kontemporer</li>
+            </ul>
+            <div class="project-modern-decor" data-caption-animate="fadeInRight" data-caption-delay="0"></div>
+          </div>
+        </div>
+        <div class="swiper-slide context-dark" data-slide-bg="images/slide-3-1339x729.jpg">
+          <div class="swiper-slide-caption project-modern">
+            <ul class="project-modern-info">
+              <li class="heading-6 project-modern-info-item project-modern-address" data-caption-animate="fadeInDown" data-caption-delay="200">Jakarta Pusat</li>
+              <li class="heading-6 project-modern-info-item" data-caption-animate="fadeInDown" data-caption-delay="100">350 m²</li>
+              <li class="heading-6 project-modern-info-item" data-caption-animate="fadeInDown" data-caption-delay="0">Hunian Mewah</li>
+            </ul>
+            <div class="project-modern-decor" data-caption-animate="fadeInRight" data-caption-delay="0"></div>
+          </div>
+        </div>
+      </div>
+      <div class="project-modern-counter">50+ Karya</div>
+      <!-- Swiper Pagination-->
+      <!--.swiper-pagination-->
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
+
