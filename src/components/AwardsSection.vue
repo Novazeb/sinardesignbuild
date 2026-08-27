3e73dc1f108f3409eacb52e6c4b31656f4489b60
Novazeb <novaberkatsyukurzebua@gmail.com>
2026-08-27 09:34:00 +0700

    feat(awards): create company achievements section with video lightbox and counters

---
+++ src/components/AwardsSection.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="section novi-background bg-cover section-lg section-inset-3 bg-white parallax-scene-js">
+    <div class="container">
+      <div class="row row-50 justify-content-md-between">
+        <div class="col-md-6">
+          <h3 class="wow fadeInRight">Konsultan Arsitektur<br>&amp; Kontraktor Terpercaya</h3>
+          <h6 class="title-style-1 wow fadeInRight" data-wow-delay=".05s">Menghadirkan Mahakarya Rancang Bangun Terbaik di Jakarta</h6>
+          <article class="video-classic wow blurIn">
+            <div class="video-classic-figure">
+              <img src="/images/index-5-533x362.jpg" alt="" width="533" height="362"/>
+            </div>
+            <a class="video-classic-link" data-lightgallery="item" href="https://youtu.be/ViSwNMJBY_Y?si=fF2VEKWuqZeQFOzJ">
+              <span class="icon mdi mdi-play"></span>
+            </a>
+          </article>
+        </div>
+        <div class="col-md-4 col-xl-3 inset-xl-top-70">
+          <div class="row row-30 row-md-50 justify-content-center justify-content-md-start">
+            <div class="col-6 col-sm-4 col-md-12 col-lg-11 wow fadeInRight">
+              <article class="counter-classic">
+                <div class="counter-classic-header">
+                  <div class="heading-1 counter-classic-number"><span class="counter">7</span></div>
+                  <h6 class="counter-classic-title">Tahun</h6>
+                </div>
+                <div class="counter-classic-text">Berpengalaman dalam industri rancang bangun arsitektur sejak 2017.</div>
+              </article>
+            </div>
+            <div class="col-6 col-sm-4 col-md-12 col-lg-11 wow fadeInRight" data-wow-delay=".05s">
+              <article class="counter-classic">
+                <div class="counter-classic-header">
+                  <div class="heading-1 counter-classic-number"><span class="counter">54</span></div>
+                  <h6 class="counter-classic-title">Proyek</h6>
+                </div>
+                <div class="counter-classic-text">Menyelesaikan puluhan proyek residensial dan komersial dengan sukses.</div>
+              </article>
+            </div>
+            <div class="col-6 col-sm-4 col-md-12 col-lg-11 wow fadeInRight" data-wow-delay=".1s">
+              <article class="counter-classic">
+                <div class="counter-classic-header">
+                  <div class="heading-1 counter-classic-number"><span class="counter">100</span><span class="counter-classic-postfix">%</span></div>
+                  <h6 class="counter-classic-title">Dedikasi</h6>
+                </div>
+                <div class="counter-classic-text">SINAR DESIGN &amp; BUILD berkomitmen penuh pada kepuasan dan kualitas klien.</div>
+              </article>
+            </div>
+            <div class="col-xl-12 wow fadeInUp">
+              <a class="button button-jerry button-primary" href="#contacts">
+                Hubungi Kami
+                <span class="button-jerry-line"></span>
+                <span class="button-jerry-line"></span>
+                <span class="button-jerry-line"></span>
+                <span class="button-jerry-line"></span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="layer-wrap layer-2">
+      <div class="layer" data-depth="0.4">
+        <img src="/images/index-8-853x574.png" alt="" width="853" height="574"/>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
+
