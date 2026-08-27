c1dfc3912ef60dc518f971a48196f126fbefe7df
Novazeb <novaberkatsyukurzebua@gmail.com>
2026-08-27 09:34:00 +0700

    feat(nav): add header navigation with SDB branding and responsive RD Navbar

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="section novi-background bg-cover page-header" id="home">
+    <!-- RD Navbar-->
+    <div class="rd-navbar-wrap">
+      <nav class="rd-navbar" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-sidebar" data-xl-device-layout="rd-navbar-sidebar" data-xxl-layout="rd-navbar-sidebar" data-xxl-device-layout="rd-navbar-sidebar" data-xl-stick-up-offset="400" data-xxl-stick-up-offset="400">
+        <div class="rd-navbar-main-outer">
+          <div class="rd-navbar-main">
+            <!-- RD Navbar Panel-->
+            <div class="rd-navbar-panel">
+              <!-- RD Navbar Brand-->
+              <div class="rd-navbar-brand">
+                <a class="brand" href="index.html">
+                  <img src="/images/logo-sdb.svg" alt="SINAR DESIGN &amp; BUILD" width="169" height="42"/>
+                </a>
+              </div>
+              <!-- RD Navbar Toggle-->
+              <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+            </div>
+            <div class="rd-navbar-main-element">
+              <div class="rd-navbar-nav-wrap">
+                <!-- RD Navbar Nav-->
+                <ul class="rd-navbar-nav">
+                  <li class="rd-nav-item active"><a class="rd-nav-link" href="#home">Beranda</a></li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="#about">Tentang Kami</a></li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="#team">Tim Kami</a></li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="#portfolio">Portofolio</a></li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="#contacts">Kontak</a></li>
+                </ul>
+                <div class="contacts-classic">
+                  <div class="unit align-items-center">
+                    <div class="unit-left"><span class="icon novi-icon mdi mdi-cellphone"></span></div>
+                    <div class="unit-body"><a class="contacts-classic-text" href="tel:+6281218939696">+62 812-1893-9696</a></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup>
+</script>
+
