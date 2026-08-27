af5d66f6d9b128ef37880ddbd8bfbc673cf778a0
Novazeb <novaberkatsyukurzebua@gmail.com>
2026-08-27 09:34:00 +0700

    feat(ui): add animated preloader component with reactive fadeout transition

---
+++ src/components/Preloader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="preloader" :class="{ loaded: isLoaded }">
+    <div class="cssload-box-loading"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const isLoaded = ref(false)
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoaded.value = true
+  }, 400)
+})
+</script>
